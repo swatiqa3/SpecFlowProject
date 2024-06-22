b072016440148e7a026ae8c6e1794649d8e596ca
Swati Pandey <swatiqa3@gmail.com>
2024-06-22 21:03:06 +0530

    masterPull

--- obj/Debug/net6.0/SpecFlowProject.AssemblyInfo.cs
+++ obj/Debug/net6.0/SpecFlowProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecFlowProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf801027b3831eaff3d64eb2cdddc0be554b516")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecFlowProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpecFlowProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
